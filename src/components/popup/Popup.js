4a1ea6197d6b97ce54b26b9a16f272c5c3943968
EugeneVe <eugene.veprytskyi@gmail.com>
2023-09-14 22:14:56 -0400

    fixed ios view

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { isIOS } from "react-device-detect";
 import "./Popup.scss";
 
 const Popup = ({ children, className }) => {
-  return <div className={`popup-wrapper ${className}`}>{children}</div>;
+  return (
+    <>
+      {!isIOS ? (
+        <div className={`popup-wrapper ${className}`}>{children}</div>
+      ) : (
+        <div className={`popup-wrapper ios ${className}`}>{children}</div>
+      )}
+    </>
+  );
 };
 
 export default Popup;
